18429e6ea70d72f2db2d7f7d840322f1ada7d72e
harsh <harshitchandel318@gmail.com>
2026-04-19 19:50:11 +0530

    feat: add SpecBuilder utility

---
+++ src/test/java/utilities/SpecBuilder.java
@@ -0,0 +1,24 @@
+package utils;
+
+import config.ConfigReader;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class SpecBuilder {
+
+    public static RequestSpecification getRequestSpec() {
+        return new RequestSpecBuilder()
+                .setBaseUri(ConfigReader.get("base.url"))
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    public static ResponseSpecification getResponseSpec() {
+        return new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+}
